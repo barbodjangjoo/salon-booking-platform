4e15d62fe3cd429b197bbf1a7979c66cf2984300
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-18 14:38:42 +0330

    cleanup models

--- backend/core/models.py
+++ backend/core/models.py
@@ -16,3 +16,36 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f'{self.first_name} - {self.last_name}'
     
+class Services(models.Model):
+    title = models.CharField(max_length=255)
+    requirement_time = models.CharField(max_length=255)
+    duration = models.PositiveIntegerField(
+        help_text='Duration in minutes'
+    )
+    svg = models.FileField(upload_to='service_svg/')
+    reserve_fee = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Staff(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    service = models.ManyToManyField(Services, on_delete=models.CASCADE)
+    min_gap_between_appointments = models.PositiveIntegerField(
+        default=0,
+        help_text='Gap between appointments in minutes'
+    )
+    
+
+class Availability(models.Model):
+    staff = models.ForeignKey( Staff, on_delete=models.CASCADE, related_name='availabilities')
+
+    date = models.DateField()
+
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.staff} - {self.date}'
