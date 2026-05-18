db3bc63e55aaae6c3180fd2a168060308e002f01
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-18 10:31:13 +0330

    implement customUser models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    phone_number = models.CharField(max_length=12)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name}'
+    
