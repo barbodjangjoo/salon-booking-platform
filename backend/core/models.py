69a15c883cee24104b8d39008c0796528556b78b
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-18 10:37:59 +0330

    update customuser model

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
 
     phone_number = models.CharField(max_length=12)
     email = models.EmailField()
+    is_phone_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.first_name} - {self.last_name}'
