df5eb2c0ae227e076b89562dfc36cc5cf18dbd88
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-20 12:49:13 +0330

    clean up models

--- backend/core/models.py
+++ backend/core/models.py
@@ -6,19 +6,22 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    phone_number = models.CharField(max_length=12)
-    email = models.EmailField()
+    phone_number = models.CharField(max_length=12, unique=True)
+    email = models.EmailField(blank=True, null=True)
     is_phone_verified = models.BooleanField(default=False)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
 
     def __str__(self):
         return f'{self.first_name} - {self.last_name}'
     
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    svg = models.FileField(upload_to='category_svg/')
+    svg = models.FileField(upload_to='category_svg/', blank=True, null=True)
     
     def __str__(self):
         return self.title
@@ -28,7 +31,7 @@ class Services(models.Model):
     title = models.CharField(max_length=255)
     requirement_time = models.CharField(max_length=255)
     duration = models.PositiveIntegerField(help_text='Duration in minutes')
-    svg = models.FileField(upload_to='service_svg/')
+    svg = models.FileField(upload_to='service_svg/', blank=True, null=True)
     reserve_fee = models.IntegerField()
 
     def __str__(self):
