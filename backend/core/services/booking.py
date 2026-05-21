f58406479017693808d5c4780c8b86afc27ae8c8
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-21 12:40:39 +0330

    update booking logic

--- backend/core/services/booking.py
+++ backend/core/services/booking.py
@@ -2,23 +2,36 @@ from datetime import datetime, timedelta
 
 from appointment.models import Appointment
 
-def generate_available_slots(availablity, service):
+
+def generate_available_slots(
+    availability,
+    service
+):
 
     slots = []
 
     service_duration = service.duration
-    gap = availablity.staff.min_gap_between_appointments
-    current_datetime = datetime.combine(
-        availablity.data,
-        availablity.start_time
+
+    gap = (
+        availability
+        .staff
+        .min_gap_between_appointments
     )
 
-    end_datetime = datetime.combine(availablity.date, availablity.end_time)
+    current_datetime = datetime.combine(
+        availability.date,
+        availability.start_time
+    )
+
+    end_datetime = datetime.combine(
+        availability.date,
+        availability.end_time
+    )
 
     reserved_appointments = Appointment.objects.filter(
-        staff = availablity.staff,
-        date = availablity.date,
-        status__in = [
+        staff=availability.staff,
+        date=availability.date,
+        status__in=[
             'pending',
             'confirmed'
         ]
@@ -35,16 +48,17 @@ def generate_available_slots(availablity, service):
             break
 
         is_reserved = reserved_appointments.filter(
-            start_time = current_datetime.time()
+            start_time=current_datetime.time()
         ).exists()
 
-        if not is_reserved:
-            slots.append({
-                'start_time': current_datetime.time(),
-                'end_time': slot_end_datetime.time()
-            })
+        slots.append({
+            'start_time': current_datetime.time(),
+            'end_time': slot_end_datetime.time(),
+            'is_available': not is_reserved
+        })
+
         current_datetime += timedelta(
             minutes=service_duration + gap
         )
-        
+
     return slots
