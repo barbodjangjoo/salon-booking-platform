0f6253954c446cb58f7be95b51de0937fc2008da
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-24 12:55:52 +0330

    add appointment list api

--- backend/appointment/urls.py
+++ backend/appointment/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('booking/', views.create_appointment_view, name='appointment_booking')
+    path('booking/', views.create_appointment_view, name='appointment_booking'),
+    path('me/', views.appointment_list_view, name='appointment_list'),
+    
 ]
