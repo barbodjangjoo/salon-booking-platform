d79b4cbec1a78d331841545efe09987ee60bedf3
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-05-24 11:52:01 +0330

    implement appointment view

--- backend/appointment/views.py
+++ backend/appointment/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+from . import models
+from . import serializers
+
+@permission_classes([IsAuthenticated])
+@api_view(['POST'])
+def create_appointment_view(request):
+    serializer = serializers.AppointmentSerializers(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    appointment = serializer.save(customer=request.user)
+
+
+
+    return Response({
+        'detail': ('Appointment created successfully!'),
+    },
+    status=status.HTTP_200_OK
+    )
