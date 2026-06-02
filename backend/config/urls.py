7e1ec1318a4fc1107fb07281860bed097fb0115e
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-02 19:34:06 +0330

    include salon to core

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/core/", include("core.urls")),
+    path("api/salon/", include("salon.urls")),
     path("api/otp/", include("otp.urls")),
 ]
