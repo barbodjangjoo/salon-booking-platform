5982960dd62ee6b4a7797f8d42f9681faf89b1db
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-02 18:32:58 +0330

    moved signals to salon

---
+++ backend/salon/signals.py
@@ -0,0 +1,21 @@
+from django.db import transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from core.models import Availability
+from appointment.tasks import generate_slots
+
+
+@receiver(post_save, sender=Availability)
+def create_slots_after_availability_created(
+    sender,
+    instance,
+    created,
+    **kwargs
+):
+    if not created:
+        return
+
+    transaction.on_commit(
+        lambda: generate_slots.delay(instance.id)
+    )
