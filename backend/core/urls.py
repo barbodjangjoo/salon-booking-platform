df2d3177a1eac8bcaedeecf7a5e042920bee41e4
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-02 18:34:30 +0330

    cleaned urls for core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('registration/', views.user_registration, name='user_register'),
 
-    path('category/', views.category_list_view, name='category_list'),
-    path('category/<int:pk>/', views.category_detail_view, name='category_detail'),
-    
 
     # path('categories/', views.category_list_view, name='category_list'),
     # path('service/<int:pk>/', views.services_detail_view, name='service_detail'),
