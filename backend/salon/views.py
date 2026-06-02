42088ba317b1c7ccc4062403352c710a3f794a91
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-02 18:18:54 +0330

    moved views

--- backend/salon/views.py
+++ backend/salon/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+from . import models
+
+@api_view(["GET"])
+def category_list_view(request):
+    qs = models.Category.objects.all()
+    serializer = serializers.CategorySerializer(qs, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def category_detail_view(request, pk):
+    category = get_object_or_404(models.Category, pk=pk)
+    serializer = serializers.CategorySerializer(category)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def service_list_view(request, pk):
+    service = get_object_or_404(
+        models.Service.objects.prefetch_related('staff'), 
+        pk=pk)
+    print(service)
+    serializer = serializers.ServiceSerializer(service)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def staff_list_view(request, pk):
+    pass
+
