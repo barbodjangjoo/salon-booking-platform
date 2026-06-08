269b6018c7b50b8a306dd87f639f8eb2f17d89d5
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-08 12:53:54 +0330

    add pep8 standard to core models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
+
 class CustomUser(AbstractUser):
     first_name = models.CharField(_('first_name'),max_length=255)
     last_name = models.CharField(_('last_name'),max_length=255)
