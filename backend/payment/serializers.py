400da8488bbbd108f2777e2e07cce3c014bd6812
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-09 17:56:43 +0330

    update factor serializers

--- backend/payment/serializers.py
+++ backend/payment/serializers.py
@@ -2,17 +2,6 @@ from rest_framework import serializers
 
 from . import models
 
-class FactorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Factor
-        fields =[
-            'id',
-            'user',
-            'payment_status',
-            'total_price',
-            'transaction_datetime',
-        ]
-
 class PurchaseItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PurchaseItem
@@ -25,3 +14,20 @@ class PurchaseItemSerializer(serializers.ModelSerializer):
             'unit_price',
             'total_price'
         ]
+
+class FactorSerializer(serializers.ModelSerializer):
+    items = PurchaseItemSerializer()
+    class Meta:
+        model = models.Factor
+        fields =[
+            'id',
+            'user',
+            'payment_status',
+            'total_price',
+            'transaction_datetime',
+            'items'
+        ]
+
+
+class AddPurchaseItem(serializers.Serializer):
+    appointment_id = serializers.CharField()
