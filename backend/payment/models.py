db6ffa397e5858352e4bcd2ccd71dd9c4736033b
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-09 10:47:50 +0330

    implement transaction

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -41,5 +41,17 @@ class PurchaseItem(models.Model):
     datetime_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user} - {self.type}"
+        return f"{self.service.title} - {self.factor}"
     
+class Transaction(models.Model):
+    factor = models.ForeignKey(Factor, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    payment_authority = models.CharField(max_length=36, blank=True, null=True)
+    code = models.IntegerField(blank=True, null=True)
+    errors = models.TextField(blank=True, null=True)
+    zarinpal_ref_id = models.CharField(max_length=150, blank=True, null=True)
+    zarinpal_data = models.TextField( blank=True, null=True)
+    payment_datetime = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return f'{self.user} - {self.factor} Transaction'
