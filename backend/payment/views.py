e162e948faa342fae40dcfd7dd1304ed30945826
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-09 11:51:27 +0330

    implement factor list view

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from . import models
+from . import serializers
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def factor_list_view(request):
+    user = request.user
+
+    qs = models.Factor.objects.filter(user=user).all()
+    serializer = serializers.FactorSerializer(qs, many=True)
+
+    return Response(serializer.data)
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def factor_detail_view(request, pk):
