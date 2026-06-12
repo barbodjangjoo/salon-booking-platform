0edadcbb5e186d8fc3fdd2f0708c682e2ae70d28
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-12 11:30:36 +0330

    bugfixed

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -6,15 +6,35 @@ from rest_framework.permissions import IsAuthenticated
 from . import models
 from . import serializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def factor_list_view(request):
     user = request.user
+    if request.method == 'GET':
+        qs = models.Factor.objects.filter(user=user).all()
+        serializer = serializers.FactorSerializer(qs, many=True)
 
-    qs = models.Factor.objects.filter(user=user).all()
-    serializer = serializers.FactorSerializer(qs, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        data = serializers.AddPurchaseItem(data=request.data)
+        data.is_valid(raise_exception=True)
+        
+        appoitment = get_object_or_404(models.Appointment, id=data.validated_data['id'])
+        if appoitment:
+            factor = models.Factor.objects.create(
+                user = user,
+                payment_status = 'pending',
+                total_price = appoitment.service.reserve_fee,
+            )
+            models.PurchaseItem.objects.create(
+                factor = factor,
+                appointment=appoitment,
+                title = 'Appointment',
+                unit_price = factor.total_price,
+                total_price = factor.total_price
+            )
 
-    return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
