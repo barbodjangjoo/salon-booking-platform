dc7b5b6748148f25cf2c31c3e9040d42c253b0f6
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-25 05:16:39 +0330

    custom user admin bugfixed

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 @admin.register(models.CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     resource_class = models.CustomUser
     list_display = [
         'id',
@@ -12,4 +12,29 @@ class CustomUserAdmin(admin.ModelAdmin):
         'phone_number',
     ]
     search_fields = ['first_name', 'last_name', 'phone_number']
+    ordering = ('id',)
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'phone_number',
+                'password1',
+                'password2',
+            ),
+        }),
+    )
