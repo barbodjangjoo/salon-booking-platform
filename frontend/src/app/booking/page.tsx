3f030108ca2dd9d742ac4cd74b9d860ad39ebd65
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-26 14:48:15 +0330

    deleted frontend folder

--- frontend/src/app/booking/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BookingPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      صفحه رزرو
-    </main>
-  );
-}
--- frontend/src/lib/api/staff.ts
+++
@@ -1,6 +0,0 @@
-import api from "./client";
-
-export const getStaffByService = async (serviceId: number) => {
-  const res = await api.get(`/core/service/${serviceId}/`);
-  return res.data;
-};
